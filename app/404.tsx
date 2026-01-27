f20b4fb726ced3c06ee938ddf4692b96316e57b4
charles806 <c08445333@gmail.com>
2026-01-27 19:47:17 +0100

    useSearchParams fix

---
+++ app/404.tsx
@@ -0,0 +1,10 @@
+// Remove this line if you have it:
+// const searchParams = useSearchParams();
+
+export default function NotFound() {
+  return (
+    <div>
+      {/* Your 404 page content */}
+    </div>
+  );
+}
